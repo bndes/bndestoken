d1c3e119aba47d4abf30e1d8f5370756a7676bf9
Suzana Maranhao <suzana@bndes.gov.br>
2019-06-06 17:34:11 -0300

    ajuste env.prd

--- Front/src/environments/environment.prod.ts
+++ Front/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   serverUrl: '/api/',
-  operationAPIURL: 'https://apigw.bndes.gov.br/tda/2.0.0/cliente/',
-  mockMongoClient: false
+  operationAPIURL: 'https://apigw-h.bndes.gov.br/tda/2.0.0/cliente/',
+  mockMongoClient: false,
+  mockMongoPJ: false  
 }
