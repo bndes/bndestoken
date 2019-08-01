801e533fa66b9f55e753e3358a05a894d9beebfb
José Nogueira <josej@bndes.gov.br>
2019-08-01 14:24:04 -0300

    setTokenAddress inside the truffle deployment script

--- Back-Blockchain/migrations/2_deploy_contracts.js
+++ Back-Blockchain/migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 var BNDESRegistry = artifacts.require("./BNDESRegistry.sol");
 var BNDESToken = artifacts.require("./BNDESToken.sol");
 
-
-module.exports = function(deployer) {
+module.exports = async (deployer) => {
+	await deployer.deploy(BNDESRegistry)
+    await deployer.deploy(BNDESToken, BNDESRegistry.address ) 
+	BNDESRegistryInstance = await BNDESRegistry.deployed();
+	BNDESTokenInstance = await BNDESToken.deployed();
+	await BNDESRegistryInstance.setTokenAddress(BNDESTokenInstance.address);	
 	
-	deployer.deploy(BNDESRegistry).then( ()=> deployer.deploy(BNDESToken, BNDESRegistry.address ) );
 };
