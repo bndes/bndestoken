8493d1a69ee377626e41562871e40d13ed43f82e
Suzana Maranhao <suzana@bndes.gov.br>
2020-01-21 15:21:52 -0300

    inicio Upgrader1

--- Back-Blockchain-Upgrade/contracts/appUpgraders/Upgrader1.sol
+++ Back-Blockchain-Upgrade/contracts/appUpgraders/Upgrader1.sol
@@ -4,56 +4,46 @@ import "../appGovernanceUpgrade/Upgrader.sol";
 import "../appGovernanceUpgrade/Governance.sol";
 import "../appGovernanceUpgrade/Storage.sol";
 
-import "./PreUpgrader3A.sol";
+import {PreUpgrader3B as LastUpgrader} from './PreUpgrader3B.sol';
 import "../LegalEntityMapping.sol";
 import "../BNDESRegistry.sol";
 
 
-contract PreUpgrader3B is Upgrader {
+contract Upgrader1 is Upgrader {
 
-    PreUpgrader3A preUpgrader3A;
+    LastUpgrader lastUpgrader;
     
     //Variables from previous Migration
     address private _governanceAddr;
     address private _resolverAddr;
-
-    //New variables 
     address private _storageContractAddr;
     address private _legalEntityMappingAddr;
     address private _bndesRegistryAddr;
 
-    constructor (address preUpgraderAddr3A) public {
-        preUpgrader3A = PreUpgrader3A(preUpgraderAddr3A);
-        _governanceAddr = preUpgrader3A.governanceAddr();
-        _resolverAddr = preUpgrader3A.resolverAddr();
-        _storageContractAddr = preUpgrader3A.storageContractAddr();
+    constructor (address lastUpgraderAddr) public {
+        lastUpgrader = LastUpgrader(lastUpgraderAddr);
+        _governanceAddr = lastUpgrader.governanceAddr();
+        _resolverAddr = lastUpgrader.resolverAddr();
+        _storageContractAddr = lastUpgrader.storageContractAddr();
+        _legalEntityMappingAddr = lastUpgrader.legalEntityMappingAddr();
+        _bndesRegistryAddr = lastUpgrader.bndesRegistryAddr();
 
     }
 
     modifier onlyGovernance() {
-        require(_governanceAddr==msg.sender, "Upgrader 3B - This function can only be executed by the Governance");
+        require(_governanceAddr==msg.sender, "Upgrader 1 - This function can only be executed by the Governance");
         _;
     }
 
     function upgrade () external onlyGovernance {
 
-        //These variables were created after by preUpgrader3A
-        _legalEntityMappingAddr = preUpgrader3A.legalEntityMappingAddr();
-        _bndesRegistryAddr = preUpgrader3A.bndesRegistryAddr();
-
         Governance governance = Governance (_governanceAddr);
         Storage storageContract = Storage(_storageContractAddr);
         LegalEntityMapping legalEntityMapping = LegalEntityMapping(_legalEntityMappingAddr);
 
-        //Very important: The governance now knows how to identify the governance members
-        governance.setIdRegistryAddr(_bndesRegistryAddr);
-
-        storageContract.addHandler(_legalEntityMappingAddr);
-        legalEntityMapping.addHandler(_bndesRegistryAddr);
-
-        Resolver resolver = Resolver(_resolverAddr);
-        resolver.changeContract("BNDESRegistry", _bndesRegistryAddr);
+        //Change a data in storage
         
+
     }
 
     function governanceAddr() public view returns (address) {
